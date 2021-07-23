731e528cc3e135f48193dcd5010b32d57f6216c2
fukaya <>
2021-07-23 19:47:47 +0900

    login_fix

---
+++ database/migrations/2021_07_23_060632_create_item_conditions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemConditionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_conditions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->unsignedBigInteger('buyer_id');
+            $table->unsignedBigInteger('secondary_category_id');
+            $table->unsignedBigInteger('item_condition_id');
+
+            // ここにカラムを追加していく
+
+            $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('users');
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('secondary_category_id')->references('id')->on('secondary_categories');
+            $table->foreign('item_condition_id')->references('id')->on('item_conditions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_conditions');
+    }
+}
